ad15f29d94790c4fa1007e5cf1d589f1b3eae255
David <drizzzydave@gmail.com>
2020-04-03 07:41:12 +0100

    Updates

---
+++ precache-manifest.6d71c889f7237a150935936c1f795fd0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d50e9136279193a529c6e1592a264961",
+    "url": "/Tudy/index.html"
+  },
+  {
+    "revision": "a2995f6f2322c8c299f7",
+    "url": "/Tudy/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "55a61e9e149cca2f1d78",
+    "url": "/Tudy/static/css/main.3e8a792f.chunk.css"
+  },
+  {
+    "revision": "a2995f6f2322c8c299f7",
+    "url": "/Tudy/static/js/2.dcdbb028.chunk.js"
+  },
+  {
+    "revision": "7ba5a91c6aa4ae9cc068a25fb13d169c",
+    "url": "/Tudy/static/js/2.dcdbb028.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55a61e9e149cca2f1d78",
+    "url": "/Tudy/static/js/main.2af00cee.chunk.js"
+  },
+  {
+    "revision": "e849b4f84c169c53f955",
+    "url": "/Tudy/static/js/runtime-main.e56d8035.js"
+  },
+  {
+    "revision": "4948a7415c322e6a95d688bec76d9d95",
+    "url": "/Tudy/static/media/personal.4948a741.png"
+  },
+  {
+    "revision": "e9c1cc96aa31a03638a2b3492db5e959",
+    "url": "/Tudy/static/media/sign.e9c1cc96.JPG"
+  }
+]);
